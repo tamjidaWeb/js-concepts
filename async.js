df30397bd3d58ac195f90de026d54777ca837467
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-06 22:25:31 +0600

    commited

--- async.js
+++ async.js
@@ -1,8 +1,6 @@
 console.log(1)
 console.log(2);
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+setTimeout(doSomething, 4000)
 console.log(5)
 console.log(6)
 
