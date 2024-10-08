faa90a0b4330fc7c80a5db464d96cedc0fa11bf0
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-08 18:16:30 +0600

    commited

---
+++ check.age.js
@@ -0,0 +1,28 @@
+// function checkAge(){
+// const ageField = document.getElementById('age');
+// const ageText = ageField.value;
+// const errorTag = document.getElementById('error')
+
+
+// try{
+//     // console.log(hello);
+//     const age = parseInt(ageText);
+//     if(isNaN(age)){
+//         throw 'please enter a number'
+//     }
+//     else if(age < 18){
+//         throw 'baccha kaccha not allowed'
+//     }
+//     else if (age > 30){
+//         throw ' murubbi'
+//     }
+// }
+// catch(err){
+//     console.log('ERROR', err);
+//     errorTag.innerHTML = 'ERROR '+ err ;
+// }
+// finally{
+//     console.log('all done inside try catch')
+// }
+// console.log(1111)
+// }
