5efb6ca6f430cb5ffddad65cf78119ca249e3435
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-08 11:31:15 +0600

    commited

--- interval.js
+++ interval.js
@@ -4,9 +4,13 @@ console.log(2);
 //     console.log(3);
 // })
 
-setTimeout(() => {
-    console.log(3)
-}, 4000);
+// setTimeout(() => {
+//     console.log(3)
+// }, 4000);
 console.log(4);
 console.log(5);
 console.log(6);
+
+setInterval(()=>{
+    console.log('i m u')
+}, 2000)
